4f2e7bc5d873749cb18b91b170a793b8faec840d
DenisseDiaz <96696742+DenisseDiaz@users.noreply.github.com>
2022-10-02 12:25:37 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // Agregar SDK Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyB0OZMmpDtfrTnWqvm6OGqfVYhnYLNvV-o",
+        authDomain: "base-pueba.firebaseapp.com",
+        databaseURL: "https://base-pueba-default-rtdb.firebaseio.com",
+        projectId: "base-pueba",
+        storageBucket: "base-pueba.appspot.com",
+        messagingSenderId: "11671325263",
+        appId: "1:11671325263:web:cb4de44d3e3bd5dbf6b83a"
+      
 };
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
